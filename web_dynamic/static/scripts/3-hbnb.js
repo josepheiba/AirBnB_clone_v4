c10a29a34db16b152544b1d46317d640e06208e8
Joseph Eiba <josepheiba@icloud.com>
2024-06-02 13:45:48 +0100

    bruh

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,21 +1,48 @@
 $(document).ready(function () {
-  $.get("http://localhost:5001/api/v1/status/", function(data){
+  $.get("http://localhost:5001/api/v1/status/", function(data) {
     if (data.status === 'OK') {
       $("div#api_status").addClass("available")
     }
   });
 
-	const selectedAmenities = {};
+  $.ajax({
+    url: 'http://localhost:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({}),
+    success: function(data) {
+      for (const place of data) {
+        const myArticle = $('<article></article>');
+        const titleBox = $('<div></div>').addClass('title_box');
+        const placeName = $('<h2></h2>').text(place.name);
+        const placePrice = $('<div></div>').addClass('price_by_night').text(place.price_by_night);
+        titleBox.append(placeName).append(placePrice);
+
+        const information = $('<div></div>').addClass('information');
+
+        const user = $('<div></div>').addClass('user');
+
+        const description = $('<div></div>').addClass('description');
+
+        myArticle.append(titleBox).append(information).append(user).append(description);
+        $('.places').append(myArticle);
+        console.log(place);
+      }
+    }
+  });
+
+  const selectedAmenities = {};
   $('input[type=checkbox]').change(function () {
     const amenityId = $(this).data('id');
     const amenityName = $(this).data('name');
     if ($(this).is(':checked')) {
       selectedAmenities[amenityId] = amenityName;
-	} else {
+    } else {
       delete selectedAmenities[amenityId];
-	}
-	console.log(selectedAmenities);
-	const amenityList = Object.values(selectedAmenities).join(', ');
+    }
+    console.log(selectedAmenities);
+    const amenityList = Object.values(selectedAmenities).join(', ');
     $('.amenities h4').text(amenityList || '\u00A0');
   });
 });
