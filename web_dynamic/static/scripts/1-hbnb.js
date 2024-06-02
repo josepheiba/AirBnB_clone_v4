3aa4bece2055b9e4e7a48ef9a0edebe1e9c45b54
Kamal <kamalmiftah01@gmail.com>
2024-06-02 03:27:10 +0100

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  $('input[type=checkbox]').change(function () {
 	const selectedAmenities = {};
+  $('input[type=checkbox]').change(function () {
     const amenityId = $(this).data('id');
     const amenityName = $(this).data('name');
     if ($(this).is(':checked')) {
@@ -8,6 +8,7 @@ $(document).ready(function () {
 	} else {
       delete selectedAmenities[amenityId];
 	}
+	console.log(selectedAmenities);
 	const amenityList = Object.values(selectedAmenities).join(', ');
     $('.amenities h4').text(amenityList || '\u00A0');
   });
